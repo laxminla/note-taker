3dde11858d071d29847c20a1eff5b2570e5eaca7
Laxmi Sharma <laxminla@yahoo.com>
2021-09-11 09:55:51 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const notes = require('./db/db.json');
+const uuid = require('uuid');
+const PORT = process.env.port || 3001;
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Static Middleware
+app.use(express.static('./public'));
+
+
+// GET Route for homepage
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './db/db.json'))
+);
+
+// Post function to add new notes to db.json
+app.post('/api/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+  const newNotes = req.body;
+  newNotes.id = uuid.v4();
+  notes.push(newNotes);
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+  res.json(notes);
+});
+
+// Delete notes function
+app.delete('/api/notes:id', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+  const deleteNote = notes.filter((rmvNote) => rmvNote.id != req.params.id)
+  fs.writeFileSync('./db/db.json', JSON.stringify(deleteNote))
+  res.json(deleteNote);
+});
+
+// Get route for homepage HTML
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// Get route for note HTML
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
+
